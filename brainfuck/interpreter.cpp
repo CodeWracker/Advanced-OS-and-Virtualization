d5637f4f0bbb91b2c547c9830b05bd44c9cb493b
CodeWracker <dev.rodrigofs@gmail.com>
2023-04-18 11:41:29 +0900

    comecando o brainfuck interpreter

---
+++ brainfuck/interpreter.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// this program implements an interpreter for the brainfuck language
+
+void fileReader(string name)
+{
+    // read the file in the tests folder
+}
+
+// main function receives the file name as an arg
+int main(int argc, char *argv[])
+{
+    // check the length of the argv
+    // if()
+
+    string name = argv[1];
+    cout << name << endl;
+
+    return 0;
+}
